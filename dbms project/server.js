fbfe17dafd1daf3e8c225652b0e68e0fa80890cd
Nandini Kataria <nandinikataria1234@gmail.com>
2024-04-15 23:57:54 +0530

    i did it haha

--- dbms project/server.js
+++ dbms project/server.js
@@ -26,38 +26,41 @@ app.get("/",function(req,res){
 })
 
 app.post("/", encoder, function(req, res){
-  var firstname = req.body.firstname;
-  var lastname = req.body.lastname;
-  var email = req.body.email;
-  var mobileno = req.body.mobileno;
-  var location = req.body.location;
-  var cycle = req.body.cycle;
+    var location = req.body['select-location'];
+    var start_datetime = req.body['start_datetime'];
+    var end_datetime = req.body['end_datetime'];
+    var userID = req.body.userID;
+    var email = req.body.email;
+    var name = req.body.name;
+    var department = req.body.department;
+    var year = req.body.year;
+    var major = req.body.major;
+    var title = req.body.title;
+    var position = req.body.position;
 
-  const selectQuery = "SELECT * FROM ContactForm WHERE first_name = ? AND last_name = ? AND email = ? AND mobile_no = ? AND selected_location = ? AND selected_cycle = ?";
-  const selectValues = [firstname, lastname, email, mobileno, location, cycle];
+    const selectQuery = "SELECT * FROM bookings WHERE location = ? AND start_datetime = ? AND end_datetime = ? AND userID = ? AND email = ? AND name = ? AND department = ? AND year = ? AND major = ? AND title = ? AND position = ?";
+    const selectValues = [location, start_datetime, end_datetime, userID, email, name, department, year, major, title, position];
 
-  connection.query(selectQuery, selectValues, function(error, results, fields){
-      if (results.length > 0) {
-          res.redirect("/welcome");
-      } else {
-          const insertQuery = "INSERT INTO ContactForm (first_name, last_name, email, mobile_no, selected_location, selected_cycle) VALUES (?, ?, ?, ?, ?, ?)";
-          const insertValues = [firstname, lastname, email, mobileno, location, cycle];
+    connection.query(selectQuery, selectValues, function(error, results, fields){
+        if (results.length > 0) {
+            res.redirect("/welcome");
+        } else {
+            const insertQuery = "INSERT INTO bookings (location, start_datetime, end_datetime, userID, email, name, department, year, major, title, position) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            const insertValues = [location, start_datetime, end_datetime, userID, email, name, department, year, major, title, position];
 
-          connection.query(insertQuery, insertValues, function(error, results, fields){
-              if (error) throw error;
-              res.redirect("/welcome");
-              res.end();
-          });
-      }
-  });
+            connection.query(insertQuery, insertValues, function(error, results, fields){
+                if (error) throw error;
+                res.redirect("/welcome");
+                res.end();
+            });
+        }
+    });
 });
-
 
 // when login is success
 app.get("/welcome",function(req,res){
-    res.sendFile(__dirname + "/welcome.html")
-})
-
+    res.sendFile(__dirname + "/index.html")
+});
 
 // set app port
 app.listen(4000);
